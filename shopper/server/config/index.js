09fef8d8154a929f3289a0fc9e9d92a82e0d6d9e
Panos Yiotis <yiotis.panos57@gmail.com>
2025-07-25 14:00:05 -0400

    Finished: Part 6: Add MySQL and Sequelize to your project (Chapter 5)

--- shopper/server/config/index.js
+++ shopper/server/config/index.js
@@ -9,4 +9,15 @@ module.exports = {
     port: 7379,
     client: null,
   },
+  mysql: {
+    options: {
+      host: "localhost",
+      port: 3406,
+      database: "shopper",
+      dialect: "mysql",
+      username: "root",
+      password: "mypassword",
+    },
+    client: null,
+  },
 };
