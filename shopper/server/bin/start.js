bc695b3bc5fa6bb6ec044db360d6dcb4500c474b
Panos Yiotis <yiotis.panos57@gmail.com>
2025-07-25 00:53:47 -0400

    Finished: Part 6: Add Redis sessions to your project (Chapter 4) p.s. had to revert connec-redis from 9 -> 5 bc syntax errors like crazy

--- shopper/server/bin/start.js
+++ shopper/server/bin/start.js
@@ -18,7 +18,6 @@ async function connectToMongoose() {
 
 function connectToRedis() {
   const redis = new Redis(config.redis.port);
-
   redis.on("connect", () => {
     console.info("Successfully connected to Redis");
   });
@@ -42,29 +41,13 @@ function onError(error) {
   if (error.syscall !== "listen") {
     throw error;
   }
-  const bind = typeof port === "string" ? `Pipe ${port}` : `Port  ${port}`;
-
-  // handle specific listen errors with friendly messages
-  switch (error.code) {
-    case "EACCES":
-      console.error(`${bind} requires elevated privileges`);
-      process.exit(1);
-      break;
-    case "EADDRINUSE":
-      console.error(`${bind} is already in use`);
-      process.exit(1);
-      break;
-    default:
-      throw error;
-  }
 }
-
 const server = http.createServer(app);
 function onListening() {
   const addr = server.address();
   const bind = typeof addr === "string" ? `pipe ${addr}` : `port ${addr.port}`;
 
-  console.info(`${config.applicationName} listening on ${bind}`);
+  console.info(`Listening on ${bind}`);
 }
 server.on("error", onError);
 server.on("listening", onListening);
